d69975e0a1933717cd8e70cf7c62078415d1f009
seonyounggg <psy8961@naver.com>
2021-07-03 21:55:43 +0900

    [Add] getPreviews API

--- src/app/dao/postDao.js
+++ src/app/dao/postDao.js
@@ -19,6 +19,48 @@ async function getBanners(connection) {
     return Rows;
 }
 
+// 미리보기 조회
+async function getPreviews(connection, categoryId, order){
+    let getPreviewsQuery;
+    if(order=='recent'){
+        getPreviewsQuery=`
+            select postId,
+               whenText as title,
+               ifnull((select imgUrl from PostImage i where p.postId = i.postId order by imgId limit 1),'') as thumbnailUrl
+            from Post p
+            where categoryId=?
+            order by createdAt desc
+            limit 4;
+        `
+    }
+    else if(order=='popular'){
+        getPreviewsQuery=`
+            select postId,
+                   whenText as title,
+                   ifnull((select imgUrl from PostImage i where p.postId = i.postId order by imgId limit 1),'') as thumbnailUrl
+            from Post p
+            where categoryId=?
+            order by (select count(*)
+                      from PostHits h
+                      where h.postId = p.postId) desc,
+                     (select avg(score) from PostStar s where s.postId = p.postId) desc,
+                     (select count(*) from PostStar s where s.postId = p.postId) desc,
+                     (select count(*) from Comment c where c.postId = p.postId) desc
+            limit 4;
+        `
+    }
+    else{
+        return null;
+    }
+
+    const [Rows] = await connection.query(
+        getPreviewsQuery,
+        [categoryId]
+    );
+    return Rows;
+}
+
 module.exports = {
     getBanners,
+    getPreviews,
 };
--- src/app/routes/postRoute.js
+++ src/app/routes/postRoute.js
@@ -3,4 +3,5 @@ module.exports = function(app){
     const jwtMiddleware = require('../../../config/jwtMiddleware');
 
     app.get('/banners', jwtMiddleware, post.getBanners);
+    app.get('/categories/:categoryId/tips', jwtMiddleware, post.getPreviews);
 };
